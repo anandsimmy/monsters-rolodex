0d96b3dbbd1394791cbda677ce057c7000c48eea
Anand Simmy <anandsimmy7@gmail.com>
2020-05-04 10:01:27 +0530

    Updates

---
+++ precache-manifest.e6426d8d2b12aacb5d11b2c49e7c0807.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8349cdc07bd9bcce396eb2af54bd2b5",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "61847f74fe883275b091",
+    "url": "/monsters-rolodex/static/css/main.05a09a84.chunk.css"
+  },
+  {
+    "revision": "dea7d94f096755c7da40",
+    "url": "/monsters-rolodex/static/js/2.ea19a0d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.ea19a0d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61847f74fe883275b091",
+    "url": "/monsters-rolodex/static/js/main.b57918b9.chunk.js"
+  },
+  {
+    "revision": "e3f9453037e5f28f8167",
+    "url": "/monsters-rolodex/static/js/runtime-main.7c0f1f0f.js"
+  }
+]);
